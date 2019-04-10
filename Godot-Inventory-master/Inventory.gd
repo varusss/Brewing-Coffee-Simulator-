aecdcdccaefe6babf7b96115a8439c981c0522c7
DKDonnor <phillipharden09@gmail.com>
2019-04-10 13:27:21 -0700

    COMMIT PLZ. Also finished with the item hotbar code.

--- Godot-Inventory-master/Inventory.gd
+++ Godot-Inventory-master/Inventory.gd
@@ -24,21 +24,18 @@ const itemImages = [
 	preload("res://Godot-Inventory-master/images/W_Sword001.png")
 ];
 
-#The dictionary that 
+#The dictionary that determines what is made in game
 const itemDictionary = {
 	0: {
 		"itemName": "Shoe",
-		"itemValue": 456,
 		"itemIcon": itemImages[3]
 	},
 	1: {
 		"itemName": "Sword",
-		"itemValue": 100,
 		"itemIcon": itemImages[7]
 	},
 	2: {
 		"itemName": "Iron Ring",
-		"itemValue": 987,
 		"itemIcon": itemImages[0]
 	},
 };
@@ -52,47 +49,16 @@ func _ready():
 	for item in itemDictionary:
 		var itemName = itemDictionary[item].itemName;
 		var itemIcon = itemDictionary[item].itemIcon;
-		var itemValue = itemDictionary[item].itemValue;
-		itemList.append(ItemClass.new(itemName, itemIcon, null, itemValue));
+		itemList.append(ItemClass.new(itemName, itemIcon, null));
 	
 	for i in range(7):
 		var slot = ItemSlotClass.new(i);
 		slotList.append(slot);
 		add_child(slot);
-	
-	slotList[0].setItem(itemList[0]);
-	slotList[1].setItem(itemList[1]);
-	slotList[2].setItem(itemList[2]);
-	
 	pass
 
-func _input(event):
-	if holdingItem != null && holdingItem.picked:
-		holdingItem.rect_global_position = Vector2(event.position.x, event.position.y);
+func _item_found(foundItem):
+	slotList[foundItem].setItem(itemList[foundItem]);
 
-func _gui_input(event):
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
-		var clickedSlot;
-		for slot in slotList:
-			var slotMousePos = slot.get_local_mouse_position();
-			var slotTexture = slot.texture;
-			var isClicked = slotMousePos.x >= 0 && slotMousePos.x <= slotTexture.get_width() && slotMousePos.y >= 0 && slotMousePos.y <= slotTexture.get_height();
-			if isClicked:
-				clickedSlot = slot;
-		
-		if holdingItem != null:
-			if clickedSlot.item != null:
-				var tempItem = clickedSlot.item;
-				var oldSlot = slotList[slotList.find(holdingItem.itemSlot)];
-				clickedSlot.pickItem();
-				clickedSlot.putItem(holdingItem);
-				holdingItem = null;
-				oldSlot.putItem(tempItem);
-			else:
-				clickedSlot.putItem(holdingItem);
-				holdingItem = null;
-		elif clickedSlot.item != null:
-			holdingItem = clickedSlot.item;
-			clickedSlot.pickItem();
-			holdingItem.rect_global_position = Vector2(event.position.x, event.position.y);
-	pass
+func _on_Button_pressed():
+	_item_found(0);
--- Godot-Inventory-master/Item.gd
+++ Godot-Inventory-master/Item.gd
@@ -3,25 +3,14 @@ extends TextureRect
 var itemIcon;
 var itemName;
 var itemSlot;
-var picked = false;
+#var picked = false;
 
-func _init(itemName, itemTexture, itemSlot, itemValue):
+func _init(itemName, itemTexture, itemSlot):
 	name = itemName;
 	self.itemName = itemName;
 	texture = itemTexture;
-	hint_tooltip = "Name: %s\n\nValue: %d" % [itemName, itemValue];
+	hint_tooltip = itemName;
 	self.itemSlot = itemSlot;
-	mouse_filter = Control.MOUSE_FILTER_PASS;
-	mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND;
-	pass
-	
-func pickItem():
-	mouse_filter = Control.MOUSE_FILTER_IGNORE;
-	picked = true;
-	pass
-	
-func putItem():
-	rect_global_position = Vector2(0, 0);
-	mouse_filter = Control.MOUSE_FILTER_PASS;
-	picked = false;
+	#mouse_filter = Control.MOUSE_FILTER_PASS;
+	#mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND;
 	pass
